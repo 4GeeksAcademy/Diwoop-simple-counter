bbb10d54b310bd26440cb8d609dc1f8d404aaa40
Diana <diana.ep.dev@gmail.com>
2024-09-18 16:26:56 +0000

    counter done

---
+++ src/js/component/counter.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+const Count = ({ seconds }) => {
+    const four = Math.floor(seconds / 1000) % 10;
+    const three = Math.floor(seconds / 100) % 10;
+    const two = Math.floor(seconds / 10) % 10;
+    const one = Math.floor(seconds / 1) % 10;
+    return (
+        <div className="container">
+            <div className="row">
+
+                <div className="col digits">{four}
+                </div>
+                <div className="col digits">{three}
+                </div>
+                <div className="col digits">{two}
+                </div>
+                <div className="col digits">{one}
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Count;
